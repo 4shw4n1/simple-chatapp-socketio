515e2c93790c7a4b750e025a35db9adba2a7d150
Ashwani Kumar Kamal <75236490+4shw4n1@users.noreply.github.com>
2021-03-27 00:31:24 +0530

    'left chat' + 'unique name'  added
    'left the chat' message will prompt in case of disconnection of a client + will delete that user from users array. Using an already used name will cause the server to ask another name.

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+//Server side JS: will handle requests and send data to all clients
+
+//importing the required modules
 const express = require('express');
 const socket = require('socket.io');
 
@@ -14,18 +17,71 @@ app.use(express.static('public'));
 //Socket setup
 const io = socket(server);
 
+//Variables to be used
+let i = 0;
 let clients = 0;
+let users = [];
+let history = [];
 
+//Handling socket events
 io.on('connection', function(socket) {
-    console.log('A user got connected');
+    const _id = socket.id;
+    console.log(_id, 'entered the chat');
     clients++;
     io.emit('client-no', {number: clients});
+
+    //in case of a disconnection
     socket.on('disconnect', function() {
-        console.log('diconnection');
+        console.log(_id, 'left the chat');
         clients--;
-        io.emit('client-no', {number: clients});
+        //To remove the user from our users array
+        for(i = 0; i < users.length; i++)
+        {
+            if(users[i].id === _id)
+            {
+                break;
+            }
+        }
+        io.emit('left-chat', users[i]); //send left the chat message
+        users.splice(i, 1);
+        io.emit('client-no', {number: clients}); //update number of clients
     });
+
+    //A new user joins
+    socket.on('enter-chat', function(data) {
+        let result = {};
+        //Handling the 'name already exists' problem
+        for(i = 0; i < users.length; i++)
+        {
+            if(users[i].name == data.name)
+            {
+                result['id'] = _id;
+                result['name'] = data.name;
+                break;
+            }
+        }
+        if(result.id != undefined)
+        {
+            io.emit('already exist', result);
+            console.log('request sent for a new name');
+        }
+        //Unique name was recieved
+        else {
+            let obj = {};
+            obj['id'] = _id;
+            obj['name'] = data.name;
+            users.push(obj);
+            io.emit('enter-chat', data);
+        }
+    });
+
+    //Sending back the chat message to all client JS instances
     socket.on('client message', function(data) {
+        if(history.length < 20) history.push(data);
+        else {
+            history.shift();
+            history.push(data);
+        }
         io.emit('client message', data);
-    })
+    });
 });
